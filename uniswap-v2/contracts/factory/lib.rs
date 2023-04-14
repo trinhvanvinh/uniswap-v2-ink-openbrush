3b5cb614502394ce36d721d14b05cf826709c770
vinhtrinhvan <trinhvanvinh018>
2023-04-14 20:30:19 +0700

    update factory

---
+++ uniswap-v2/contracts/factory/lib.rs
@@ -0,0 +1,40 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![feature(min_specialization)]
+
+use core::hash::Hash;
+
+use ink::primitives::AccountId;
+use openbrush::traits::ZERO_ADDRESS;
+
+#[openbrush::contract]
+pub mod factory {
+    use openbrush::traits::{
+        Storage,
+        ZERO_ADDRESS,
+    };
+
+    use uniswap_v2::{
+        impls::factory::*,
+        traits::factory::*,
+    };
+
+    #[ink(storage)]
+    #[derive(Default, Storage)]
+    pub struct FactoryContract {
+        #[storage_field]
+        factory: data::Data,
+    }
+
+    impl Factory for FactoryContract {}
+}
+
+impl FactoryContract {
+    #[ink(constructor)]
+    pub fn new(fee_to_setter: AccountId, pair_code_hash: Hash) -> Self {
+        let mut instance = Self::default();
+        instance.factory.pair_contract_code_hash = pair_code_hash;
+        instance.factory.fee_to_setter = fee_to_setter;
+        instance.factory.fee_to = ZERO_ADDRESS.into();
+        instance
+    }
+}
---
+++ uniswap-v2/logics/impls/factory/data.rs
@@ -0,0 +1,34 @@
+use ink::{
+    prelude::vec::Vec,
+    primitives::Hash,
+};
+
+use openbrush::{
+    storage::Mapping,
+    traits::{
+        AccountId,
+        ZERO_ADDRESS,
+    },
+};
+
+pub const STORAGE_KEY: u32 = openbrush::storage_unique_key!(Data);
+
+pub struct Data {
+    pub fee_to: AccountId,
+    pub fee_to_setter: AccountId,
+    pub get_pair: Mapping<(AccountId, AccountId), AccountId>,
+    pub all_pairs: Vec<AccountId>,
+    pub pair_contract_code_hash: Hash,
+}
+
+impl Default for Data{
+    fn default() -> Self{
+        Self{
+            fee_to: ZERO_ADDRESS.into(),
+            fee_to_setter: ZERO_ADDRESS.into(),
+            get_pair: Default::default(),
+            all_pairs: Default::default(),
+            pair_contract_code_hash: Default::default()
+        }
+    }
+}
